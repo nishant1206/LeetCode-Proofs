99ada79f41ea0d2bb2d2e55ba1db72b95066d70a
nishant1206 <72695913+nishant1206@users.noreply.github.com>
2025-05-30 01:15:53 +0530

    Update Problem2.cpp

--- Problem2.cpp
+++ Problem2.cpp
@@ -52,3 +52,60 @@ Constraints:
 1 <= nums[i] <= 109
 nums consists of distinct positive integers.
 */
+
+/* Sollution */
+
+class Solution {
+public:
+    
+    int digitSum(int n) {
+        int sum = 0;
+        while (n) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    int minSwaps(vector<int>& nums) {
+          int n = nums.size();
+
+    
+    vector<tuple<int, int, int>> arr;
+    for (int i = 0; i < n; ++i) {
+        arr.emplace_back(digitSum(nums[i]), nums[i], i);
+    }
+
+    
+    sort(arr.begin(), arr.end());
+
+    
+    vector<bool> visited(n, false);
+    unordered_map<int, int> indexMap; 
+    for (int i = 0; i < n; ++i) {
+        int origIndex = get<2>(arr[i]);
+        indexMap[origIndex] = i;
+    }
+
+    
+    int swaps = 0;
+    for (int i = 0; i < n; ++i) {
+        if (visited[i] || indexMap[i] == i)
+            continue;
+
+        int cycleSize = 0;
+        int j = i;
+        while (!visited[j]) {
+            visited[j] = true;
+            j = indexMap[j];
+            cycleSize++;
+        }
+
+        if (cycleSize > 0) {
+            swaps += (cycleSize - 1);
+        }
+    }
+
+    return swaps;
+    }
+};
