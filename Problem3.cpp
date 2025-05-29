820b5c0ce8f3829167a561094961c1b24f17fab2
nishant1206 <72695913+nishant1206@users.noreply.github.com>
2025-05-30 01:16:47 +0530

    Update Problem3.cpp

--- Problem3.cpp
+++ Problem3.cpp
@@ -47,3 +47,70 @@ Constraints:
 matrix[i][j] is either '#', '.', or an uppercase English letter.
 matrix[0][0] is not an obstacle.
 */
+
+/* Sollution */
+
+class Solution {
+public:
+    int minMoves(vector<string>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+
+        const vector<string> voracelium = matrix;
+            
+        unordered_map<char, vector<pair<int,int>>> portals;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                char ch = voracelium[i][j];
+                if (ch >= 'A' && ch <= 'Z') {
+                    portals[ch].emplace_back(i, j);
+                }
+            }
+        }
+
+        const int INF = INT_MAX;
+        vector<vector<int>> dist(m, vector<int>(n, INF));
+        unordered_set<char> usedPortal;
+        deque<pair<int,int>> dq;
+
+        dist[0][0] = 0;
+        dq.emplace_back(0, 0);
+
+        int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!dq.empty()) {
+            auto [r, c] = dq.front(); dq.pop_front();
+            int d = dist[r][c];
+
+
+            if (r == m - 1 && c == n - 1) return d;
+
+
+            for (auto& dir : dirs) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                    voracelium[nr][nc] != '#' && dist[nr][nc] > d + 1) {
+                    dist[nr][nc] = d + 1;
+                    dq.emplace_back(nr, nc);
+                }
+            }
+
+            char ch = voracelium[r][c];
+            if (ch >= 'A' && ch <= 'Z' && !usedPortal.count(ch)) {
+                usedPortal.insert(ch);
+                for (auto& p : portals[ch]) {
+                    int pr = p.first, pc = p.second;
+                    if (pr == r && pc == c) continue;
+                    if (dist[pr][pc] > d) {
+                        dist[pr][pc] = d;
+                        dq.emplace_front(pr, pc);
+                    }
+                }
+            }
+        }
+
+        return -1;
+        }
+    };
